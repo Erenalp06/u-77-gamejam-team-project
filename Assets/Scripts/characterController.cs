a4c2afe7c307369345d51221131ee3b4a95b6d40
Agah <agahgames260@gmail.com>
2023-04-09 16:08:34 +0300

    Update characterController.cs

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -13,13 +13,13 @@ public class characterController : MonoBehaviour
     private bool isGrounded, jumping, isWalkable = true;
 
     public bool hurted;
-    
+
     private SpriteRenderer spriteRenderer;
 
     private Animator animator;
-    
+
     [SerializeField] private bool isGrounded, jumping;
-    
+
     public LayerMask interactableLayer;
 
 
@@ -37,69 +37,82 @@ public class characterController : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
-    void Update(){
-        if(Input.GetAxisRaw("Horizontal") < 0){
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
             spriteRenderer.flipX = true;
-        }else{
+        }
+        else
+        {
             spriteRenderer.flipX = false;
         }
 
-        if(isGrounded){
+        if (isGrounded)
+        {
             jumping = true;
         }
 
         animator.SetBool("hurt", hurted);
 
-        
 
-        
+
+
     }
-    private void Interact(){
-            NPCController.Instance.Interact();
+    private void Interact()
+    {
+        NPCController.Instance.Interact();
     }
     public void HandleUpdate()
 
-    {          
-
     {
 
-        
-
-        if(isWalkable && !hurted){
-
-            float moveInput = Input.GetAxisRaw("Horizontal");
-            animator.SetFloat("speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
-            rigidbody2D.velocity = new Vector2(moveInput * moveSpeed, rigidbody2D.velocity.y);
-
-            if(Input.GetKey(KeyCode.Space) && isGrounded){
-                isGrounded = false;         
-
-                print("jump");
-                animator.SetTrigger("jump");
-                rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);      
-
-            print("jump");
-            AudioManager.Instance.PlaySFX("jump");
-            animator.SetTrigger("jump");
-            rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            jumping = false;
+        {
 
 
+
+            if (isWalkable && !hurted)
+            {
+
+                float moveInput = Input.GetAxisRaw("Horizontal");
+                animator.SetFloat("speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+                rigidbody2D.velocity = new Vector2(moveInput * moveSpeed, rigidbody2D.velocity.y);
+
+                if (Input.GetKey(KeyCode.Space) && isGrounded)
+                {
+                    isGrounded = false;
+
+                    print("jump");
+                    animator.SetTrigger("jump");
+                    rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+                    print("jump");
+                    AudioManager.Instance.PlaySFX("jump");
+                    animator.SetTrigger("jump");
+                    rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                    jumping = false;
+
+
+                }
+
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    Interact();
+                }
+                animator.SetBool("isGrounded", isGrounded);
             }
-        
-         if(Input.GetKeyDown(KeyCode.Z)){
-                     Interact();
-         }
-        animator.SetBool("isGrounded", isGrounded);
-    }
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.gameObject.CompareTag("ground")){
-            isGrounded = true;
-        }
+            void OnCollisionEnter2D(Collision2D other)
+            {
+                if (other.gameObject.CompareTag("ground"))
+                {
+                    isGrounded = true;
+                }
 
-        if(other.gameObject.CompareTag("npc")){                       
-            other.gameObject.GetComponent<Collider2D>().enabled = false;                      
+                if (other.gameObject.CompareTag("npc"))
+                {
+                    other.gameObject.GetComponent<Collider2D>().enabled = false;
+                }
+            }
         }
-    }  
+    }
 }
