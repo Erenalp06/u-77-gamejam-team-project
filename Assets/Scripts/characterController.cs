33cc4632c6aa592d3f40bec8014e8753575b3a37
Agah <agahgames260@gmail.com>
2023-04-09 07:47:18 +0300

    Temizlik, Kod ve Dino Animasyonlarında
    Dino Animasyonları silindi, characterController temizlendi, fikri bir 2. level oluşturuldu, bir player prefabi oluşturuldu.

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -7,96 +7,61 @@ public class characterController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
 
-    
 
     private Rigidbody2D rigidbody2D;
-    private bool isGrounded, jumping, isWalkable = true;
-    
 
     private SpriteRenderer spriteRenderer;
 
     private Animator animator;
-
+    
+    [SerializeField] private bool isGrounded, jumping;
+    
     public LayerMask interactableLayer;
+
+
 
     public static characterController Instance;
 
-    
+
     void Awake()
     {
         Instance = this;
     }
-
-    
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
-
     void Update(){
-
         if(Input.GetAxisRaw("Horizontal") < 0){
             spriteRenderer.flipX = true;
         }else{
             spriteRenderer.flipX = false;
         }
-
-        if(isGrounded){
-            jumping = true;
-        }
-
         
-
-        
-
-        
-
     }
-
     private void Interact(){
             NPCController.Instance.Interact();
-        }
-
-    
-
-    
+    }
     public void HandleUpdate()
-    {    
-
-        if(isWalkable){
+    {          
             float moveInput = Input.GetAxisRaw("Horizontal");
             animator.SetFloat("speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
             rigidbody2D.velocity = new Vector2(moveInput * moveSpeed, rigidbody2D.velocity.y);
 
-            if(Input.GetKey(KeyCode.Space)){
-            if(jumping){
+            if(Input.GetKey(KeyCode.Space) && isGrounded){
                 isGrounded = false;         
-            print("jump");
-            animator.SetTrigger("jump");
-            rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            jumping = false;
-
+                print("jump");
+                animator.SetTrigger("jump");
+                rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);      
             }
-            
-        }
-
-        if(Input.GetKeyDown(KeyCode.Z)){
-            Interact();
-        }
-        }
-
         
-        
-
+         if(Input.GetKeyDown(KeyCode.Z)){
+                     Interact();
+         }
         animator.SetBool("isGrounded", isGrounded);
-        
     }
-
-    
-
-    
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("ground")){
@@ -104,20 +69,7 @@ public class characterController : MonoBehaviour
         }
 
         if(other.gameObject.CompareTag("npc")){                       
-            other.gameObject.GetComponent<Collider2D>().enabled = false;
-            
-            
-                       
+            other.gameObject.GetComponent<Collider2D>().enabled = false;                      
         }
-
-        
-
-        
-        
-    }
-    
-    
-
-    
-    
+    }  
 }
